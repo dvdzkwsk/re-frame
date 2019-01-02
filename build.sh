b29cdd41e26d0b9cd933d4dd0c64c6682eb5beb0
David Zukowski <david@zuko.me>
2019-01-02 13:37:35 -0800

    Create development and production builds

--- build.sh
+++ build.sh
@@ -1,2 +1,31 @@
+#!/bin/bash
+clean () {
+  rm -rf dist
+}
+
+on_error () {
+  err_code=$?
+  clean
+  exit $err_code
+}
+
+# handle any error that occurs
+trap on_error ERR
+
+# remove any previously-built artifcts
+clean
+
+# bundle the library
 yarn rollup lib/index.js --format esm --file dist/re-frame.js
-yarn uglifyjs --compress --mangle --output dist/re-frame.min.js dist/re-frame.js
+
+# create development bundle
+cp dist/re-frame.js dist/development.js
+sed -i 's/__DEV__/true/g' dist/development.js
+
+# create production bundle
+cp dist/re-frame.js dist/production.js
+sed -i 's/__DEV__/false/g' dist/production.js
+yarn uglifyjs --compress --mangle --output dist/production.js dist/production.js
+
+# remove the original bundle, since we just want development and production variants
+rm dist/re-frame.js
