efdadcd0fddea0f98221df8c9883abeeed6a875b
David Zukowski <david@zuko.me>
2019-01-08 16:24:56 -0800

    Implement "dispatchSync"

--- test/create-store.spec.js
+++ test/create-store.spec.js
@@ -1,7 +1,7 @@
 import test from 'ava'
 import * as reframe from '../lib/index.js'
 
-function flush() {
+function processDispatchedEvents() {
   return new Promise(resolve => setTimeout(resolve))
 }
 
@@ -16,15 +16,15 @@ test('dispatch > EventDB handler updates DB state', async t => {
   store.registerEventDB('double', db => db * 2)
 
   store.dispatch(['double'])
-  await flush()
+  await processDispatchedEvents()
   t.is(store.snapshot().db, 2)
 
   store.dispatch(['double'])
-  await flush()
+  await processDispatchedEvents()
   t.is(store.snapshot().db, 4)
 
   store.dispatch(['double'])
-  await flush()
+  await processDispatchedEvents()
   t.is(store.snapshot().db, 8)
 })
 
@@ -35,6 +35,73 @@ test('dispatch > throws if called with an unwrapped string', async t => {
   }, 'You dispatched an invalid event. An event is an array that looks like [eventId] or [eventId, payload].')
 })
 
+test('dispatch > does not process the event immediately', async t => {
+  const processedEvents = []
+  const store = reframe.createStore()
+  store.registerEventDB('a', (db, event) => processedEvents.push(event))
+
+  store.dispatch(['a'])
+  t.deepEqual(processedEvents, [])
+  await processDispatchedEvents()
+  t.deepEqual(processedEvents, [['a']])
+})
+
+test('dispatch > processes events dispatched in the same loop as batch', async t => {
+  const processedEvents = []
+  const store = reframe.createStore()
+  store.registerEventDB('a', (db, event) => processedEvents.push(event))
+  store.registerEventDB('b', (db, event) => processedEvents.push(event))
+  store.registerEventDB('c', (db, event) => processedEvents.push(event))
+
+  store.dispatch(['a'])
+  store.dispatch(['b'])
+  store.dispatch(['c'])
+  t.deepEqual(processedEvents, [])
+
+  await processDispatchedEvents()
+  t.deepEqual(processedEvents, [['a'], ['b'], ['c']])
+})
+
+test('dispatch > processes events in the order they are dispatched', async t => {
+  const processedEvents = []
+  const store = reframe.createStore()
+  store.registerEventDB('a', (db, event) => processedEvents.push(event))
+  store.registerEventDB('b', (db, event) => processedEvents.push(event))
+  store.registerEventDB('c', (db, event) => processedEvents.push(event))
+
+  store.dispatch(['a'])
+  store.dispatch(['b'])
+  store.dispatch(['c'])
+
+  await processDispatchedEvents()
+
+  t.deepEqual(processedEvents, [['a'], ['b'], ['c']])
+})
+
+test('dispatchSync > processes an event synchronously', t => {
+  const processedEvents = []
+  const store = reframe.createStore()
+  store.registerEventDB('a', (db, event) => processedEvents.push(event))
+  store.dispatchSync(['a'])
+  t.deepEqual(processedEvents, [['a']])
+})
+
+test('dispatchSync > processes an event ahead of the existing queue', async t => {
+  const processedEvents = []
+  const store = reframe.createStore()
+  store.registerEventDB('async', (db, event) => processedEvents.push(event))
+  store.registerEventDB('sync', (db, event) => processedEvents.push(event))
+
+  store.dispatch(['async'])
+  store.dispatch(['async'])
+  store.dispatch(['async'])
+  store.dispatchSync(['sync'])
+
+  t.deepEqual(processedEvents, [['sync']])
+  await processDispatchedEvents()
+  t.deepEqual(processedEvents, [['sync'], ['async'], ['async'], ['async']])
+})
+
 test('snapshot > returns an object containing the current state', async t => {
   const store = reframe.createStore(0)
   store.registerEventDB('increment', db => db + 1)
@@ -42,13 +109,13 @@ test('snapshot > returns an object containing the current state', async t => {
   store.dispatch(['increment'])
   store.dispatch(['increment'])
   store.dispatch(['increment'])
-  await flush()
+  await processDispatchedEvents()
   const snapshot = store.snapshot() // should be 3
 
   store.dispatch(['increment'])
   store.dispatch(['increment'])
   store.dispatch(['increment'])
-  await flush()
+  await processDispatchedEvents()
 
   t.is(snapshot.db, 3)
   t.is(store.snapshot().db, 6)
@@ -62,7 +129,7 @@ test('snapshot > can restore store to snapshotted state', async t => {
   store.dispatch(['increment'])
   store.dispatch(['increment'])
   store.dispatch(['increment'])
-  await flush()
+  await processDispatchedEvents()
 
   t.is(store.snapshot().db, 3)
   snapshot.restore()
