778e680ad5c3e67fad52176050fc4769b15ef624
David Zukowski <david@zuko.me>
2019-04-04 09:58:10 -0700

    Remove "const" usage from @re-frame/global

--- packages/global/lib/global.js
+++ packages/global/lib/global.js
@@ -2,18 +2,18 @@ import {createStore} from "@re-frame/store"
 export * from "@re-frame/interceptors"
 export * from "@re-frame/effects"
 
-const store = createStore()
+var store = createStore()
 
-export const getState = store.getState
-export const dispatch = store.dispatch
-export const dispatchSync = store.dispatchSync
-export const query = store.query
-export const subscribe = store.subscribe
-export const injectCoeffect = store.injectCoeffect
-export const registerCoeffect = store.registerCoeffect
-export const registerEventDB = store.registerEventDB
-export const registerEventFX = store.registerEventFX
-export const registerEffect = store.registerEffect
-export const registerSubscription = store.registerSubscription
-export const addPostEventCallback = store.addPostEventCallback
-export const removePostEventCallback = store.removePostEventCallback
+export var getState = store.getState
+export var dispatch = store.dispatch
+export var dispatchSync = store.dispatchSync
+export var query = store.query
+export var subscribe = store.subscribe
+export var injectCoeffect = store.injectCoeffect
+export var registerCoeffect = store.registerCoeffect
+export var registerEventDB = store.registerEventDB
+export var registerEventFX = store.registerEventFX
+export var registerEffect = store.registerEffect
+export var registerSubscription = store.registerSubscription
+export var addPostEventCallback = store.addPostEventCallback
+export var removePostEventCallback = store.removePostEventCallback
