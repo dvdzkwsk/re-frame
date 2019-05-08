c04d86f23783e9d3638c2ef5b8871b6b03d18d83
David Zukowski <dazukows@microsoft.com>
2019-05-08 16:24:08 -0700

    Reorganize extension

--- packages/extension/panel/create-panel.js
+++
@@ -1,5 +0,0 @@
-chrome.devtools.panels.create(
-  "Re-frame",
-  "/images/re-frame-logo.png",
-  "/panel/re-frame.panel.html"
-)
---
+++ packages/extension/public/re-frame.create-panel.js
@@ -0,0 +1,5 @@
+chrome.devtools.panels.create(
+  "Re-frame",
+  "/public/images/re-frame-logo.png",
+  "/public/re-frame.panel.html"
+)
